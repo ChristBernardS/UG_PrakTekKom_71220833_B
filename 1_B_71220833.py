0eec15ef1abef117d73d3b2a684c7aeeeea1a8bf
Christ <117885718+ChristBernardS@users.noreply.github.com>
2022-12-08 13:02:00 +0700

    Delete 1_B_71220833.py

--- 1_B_71220833.py
+++
@@ -1,8 +0,0 @@
-print('SELAMAT DATANG DI PROGRAM PEMBUATAN PIRAMIDA BERLUBANG')
-angka = int(input('Masukan Angka : '))
-for i in range(0, angka-1, 1):
-    print((' '*(angka-i))+('*'*(i-(i-1))), end='')
-    for j in range(1):
-        print('*', end=' ')
-    print('')
-print((' '+'*'*(angka))+('*'*(angka-1)))
